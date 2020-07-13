9b95a2d039aa11e978057b58cd7a579447a89b5a
natanguedes <ngsneto@gmail.com>
2020-07-13 11:52:44 -0300

    Variáveis, tipos e estruturas de dados

---
+++ CAP2-Python/variaveis-tipos-e-estruturas.py
@@ -0,0 +1,70 @@
+
+#pythonFundamentos
+
+
+
+#identação tab ou barra de espaço 2x
+
+
+
+
+#examples
+
+print(nivel 1) #primeiro nível  hieráquio
+
+if(True):
+    print(nivel 2) #segundo nível hierárquico
+
+#Block0
+    #Header-line
+            #Block1
+            #Header-line
+                  #Block2
+            #Block1
+#Block0
+
+
+
+    #comentários em Python
+  #Começam com caracter    # ou 3 aspas duplas """ """
+
+  #isso é um comentário em uma única linha
+    """
+    isso é um comentário em mais de uma linha 
+    """
+"""
+    Dicas
+    clareza é importante . Mantenha seu código limpo e organizado
+    Código esparso é melhor que código  denso
+    sempre documente seu código
+    siga os padrões não para criar complexidade , mas para manter uma regra
+    erros nunca serão silkenciosos, a menos que propositalmente
+    simples é melhor que complexo e complexo é  melhor que  complicado
+    Não se sinta obrigado a criar classes sem uma boa razão
+"""
+
+Números e matemáticas
+
+sumário
+
+1-tipos de numeros em python
+2-Funções e operações com numeros
+3- Aritmética
+4-Operadores
+
+
+tipos principais de numeros em python: int float
+int-> numeros inteiros, positivos ou negativos EX: -7 e 7
+float ->números fracionários,positivos ou negativos Ex: -7.1 e 7.1
+
+#função type para saber qual é o tipo de um número
+#função Int() ou float() para converter números
+
++ = 2+2=4
+- = 2-2=0
+* = 3*3=9
+/ = 4/4= 1
+% = modulo  = 5 % 2 = 1
+** potencia =  4**2 = 16
+int() = converte inteiro = int(3,2) =
+float() = converte float = float(2) = 2.0
